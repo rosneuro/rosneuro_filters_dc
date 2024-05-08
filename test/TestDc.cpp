19becb3538119e746d3ee26dbf0c47ad20649d1e
mirovix <miroljubmihailovic98@gmail.com>
2024-05-08 17:04:52 +0200

    test: integration test

--- test/TestDc.cpp
+++ test/TestDc.cpp
@@ -1,46 +1,71 @@
 #include "Dc.hpp"
 #include <gtest/gtest.h>
+#include <ros/package.h>
+#include "rosneuro_filters/rosneuro_filters_utilities.hpp"
 
 namespace rosneuro {
+    template <typename T>
+    class DcTest : public Dc<T> {
+        public:
+            DcTest() : Dc<T>() {};
+            ~DcTest() {};
+    };
 
-template <typename T>
-class DcTest : public Dc<T> {
-    public:
-        DcTest() : Dc<T>() {};
-        ~DcTest() {};
-};
+    class DcTestSuite : public ::testing::Test {
+        public:
+            DcTestSuite() {};
+            ~DcTestSuite() {};
+            virtual void SetUp() { dc = new DcTest<double>(); };
+            virtual void TearDown() { delete dc; };
+            DcTest<double>* dc;
+    };
 
-class DcTestSuite : public ::testing::Test {
-    public:
-        DcTestSuite() {};
-        ~DcTestSuite() {};
-        virtual void SetUp() { dc = new DcTest<double>(); };
-        virtual void TearDown() { delete dc; };
-        DcTest<double>* dc;
-};
+    TEST_F(DcTestSuite, Constructor) {
+        ASSERT_EQ(dc->name_, "dc");
+    }
 
-TEST_F(DcTestSuite, Constructor) {
-    ASSERT_EQ(dc->name_, "dc");
-}
+    TEST_F(DcTestSuite, Configure) {
+        ASSERT_TRUE(dc->configure());
+    }
 
-TEST_F(DcTestSuite, Configure) {
-    ASSERT_TRUE(dc->configure());
-}
+    TEST_F(DcTestSuite, Apply) {
+        Eigen::Matrix<double, 3, 3> input_matrix;
+        input_matrix << 1.0, 2.0, 3.0,
+                        4.0, 5.0, 6.0,
+                        7.0, 8.0, 9.0;
 
-TEST_F(DcTestSuite, Apply) {
-    Eigen::Matrix<double, 3, 3> input_matrix;
-    input_matrix << 1.0, 2.0, 3.0,
-                    4.0, 5.0, 6.0,
-                    7.0, 8.0, 9.0;
+        auto result_matrix = dc->apply(input_matrix);
+        ASSERT_EQ(result_matrix.rows(), 3);
+        ASSERT_EQ(result_matrix.cols(), 3);
 
-    auto result_matrix = dc->apply(input_matrix);
-    ASSERT_EQ(result_matrix.rows(), 3);
-    ASSERT_EQ(result_matrix.cols(), 3);
+        for (int i = 0; i < result_matrix.cols(); ++i)
+            ASSERT_NEAR(result_matrix.col(i).mean(), 0.0, 1e-6);
+    }
 
-    for (int i = 0; i < result_matrix.cols(); ++i)
-        ASSERT_NEAR(result_matrix.col(i).mean(), 0.0, 1e-6);
-}
+    TEST_F(DcTestSuite, Integration) {
+        ASSERT_TRUE(dc->configure());
 
+        int frame_size = 32;
+        std::string base_path = ros::package::getPath("rosneuro_filters_dc");
+
+        const std::string input_path = base_path + "/test/rawdata.csv";
+        const std::string output_path = base_path + "/test/expected.csv";
+
+        rosneuro::DynamicMatrix<double> input = readCSV<double>(input_path);
+        rosneuro::DynamicMatrix<double> expected = readCSV<double>(output_path);
+
+        int nsamples  = input.rows();
+        int nchannels = input.cols();
+
+        rosneuro::DynamicMatrix<double> output = rosneuro::DynamicMatrix<double>::Zero(nsamples, nchannels);
+        rosneuro::DynamicMatrix<double> frame = rosneuro::DynamicMatrix<double>::Zero(frame_size, nchannels);
+
+        for(auto i = 0; i<nsamples; i = i+frame_size) {
+            frame = input.middleRows(i, frame_size);
+            output.middleRows(i, frame_size) = dc->apply(frame);
+        }
+        ASSERT_TRUE(expected.isApprox(output, 1e-6));
+    }
 }
 
 int main(int argc, char **argv) {
